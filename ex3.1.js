b65f54fba566b97b3e58a34212afa0c7be470637
foadal99 <57833439+foadal99@users.noreply.github.com>
2021-02-20 14:25:17 +0200

    Update ex3.1.js

--- ex3.1.js
+++ ex3.1.js
@@ -1,10 +1,12 @@
 function nb_year(p0,percent,aug,p){
-  count=0;
+for(var i=0;p>p0;i++,p0+=(percent/100)*p0+aug);
+return i;
+  /*count=0;
   while(p>p0){
     p0+=(percent/100)*p0+aug;
     count++
   }
-  return count;
+  return count;*/
 }
 
 console.log(nb_year(1500000, 2.5, 10000, 2000000));
