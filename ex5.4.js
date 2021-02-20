d8d7023e94d901f1eca71b234de6f930a66844c1
foadal99 <57833439+foadal99@users.noreply.github.com>
2021-02-20 14:12:13 +0200

    in one line

--- ex5.4.js
+++ ex5.4.js
@@ -1,11 +1,12 @@
 function WeirdCase(string){
-  var str=[];
-  string.split("").forEach((element,i) => {
+  let str=string.split("");
+  return string.split("").map((val,i)=>i%2?val.toLowerCase() :val.toUpperCase()).join('')
+  /*string.split("").forEach((element,i) => {
     if(i%2==0){
       return str.push(element.toUpperCase());
     }
     return str.push(element.toLowerCase());
   });
-  return str.join('')
+  return str.join('')*/
 }
 console.log(WeirdCase("Weird string case"));
