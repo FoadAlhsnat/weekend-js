90f71d40c76db1f1dfe545496fbe12a94294d905
foadal99 <57833439+foadal99@users.noreply.github.com>
2021-02-20 19:47:43 +0200

    Update ex4.1.js

--- ex4.1.js
+++ ex4.1.js
@@ -1,3 +1,14 @@
-const Fibonacci=num=>num<=2?1:Fibonacci(num-1)+Fibonacci(num-2)
+const Fibonacci=num=>num<=2?1:Fibonacci(num-1)+Fibonacci(num-2)//return sum
 
 console.log(Fibonacci(8));
+
+
+
+
+const Fibonacci=num=>{//return array
+  let arr=[1,1]
+  for (let i = 1; i <= num; i++) {
+    arr.push(arr[i]+arr[i-1])
+  return arr;
+}
+}
